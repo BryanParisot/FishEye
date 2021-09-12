143641f6104680c59cc52be5e4375093007e0b10
Bryan <bryanparisot@hotmail.com>
2021-09-12 15:29:36 +0200

    :memo: add photographe unique file html

--- js/home/displayHome.js
+++ js/home/displayHome.js
@@ -9,17 +9,19 @@ export default class displayHome {
         const containMain = document.getElementById("contain_cards");
         create.className = "articlePh";
         let templatePhotographer = `
-        <a href="#" title="">
-          <img src=${photographer.portrait} alt="" />
-          <h2 class="name">${photographer.name}</h2>
-        </a>
-        <p class="city">${photographer.city}, ${photographer.country}</p>
-        <p class="tagline">${photographer.tagline}</p>
-        <span class="price">${photographer.price}/jours</span>
-        <ul class="filter">${photographer.tags
-          .map((item) => `<li>#${item}</li>`)
-          .join(" ")}</ul>
-
+      <a href="#" title=${photographer.name}>
+        <img src="${photographer.portrait}" alt="" />
+        <h2 class="name">${photographer.name}</h2>
+      </a>
+      <p class="city">${photographer.city}, ${photographer.country}</p>
+      <p class="tagline">${photographer.tagline}</p>
+      <span class="price">${photographer.price}/jours</span>
+      <ul class="filter">
+        ${photographer.tags .map((item) => `
+        <li>#${item}</li>
+        `) .join(" ")}
+      </ul>
+      
             `;
         containMain.appendChild(create);
         create.innerHTML = templatePhotographer;
