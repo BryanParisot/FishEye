895359ae5c0e26bf58cfbab82c2b341c7656969b
Bryan <bryanparisot@hotmail.com>
2021-10-15 09:29:13 +0200

    Display photographer and filter photographe

--- js/photographeUnique/profil.js
+++ js/photographeUnique/profil.js
@@ -1,9 +1,10 @@
 import dataApi from "../dataUser/dataApi.js";
+import listPhotos from "./listPhotos.js";
+import form from "./formulaire.js";
 
 export default class profil {
   displayProfil() {
     new dataApi().findData().then((response) => {
-      console.log(response);
       response.photographer.map((photographer) => {
         let id = window.location.search.split("id=")[1];
         if (id != photographer.id) {
@@ -12,35 +13,89 @@ export default class profil {
         }
         const createProfilUnique = document.createElement("div");
         const profil = document.getElementById("aaa");
+        const namePh = document.getElementById("namePh");
         createProfilUnique.className = "container_description_profil";
-        let contenuHtml = `      
-        <div class="container_description_flex">
-        <div class="container_for_width">
-          <h2 class="name_profil">${photographer.name}</h2>
-          <p class="ville_profil">${photographer.city}, ${
-          photographer.country
-        }</p>
-          <p class="txt_profil">${photographer.tagline}</p>
-          <ul class="filter">
-          ${photographer.tags
-            .map(
-              (item) => `
-          <li>#${item}</li>
-          `
-            )
-            .join(" ")}
-        </ul>
-          </div>
-          <div class="container_btn"><button>Contactez-moi</button></div>
-          </div>
-      <div class="container_img">
-        <img src=${photographer.portrait} alt="" />
-      </div>`;
 
-        console.log(photographer.name);
         profil.appendChild(createProfilUnique);
-        createProfilUnique.innerHTML = contenuHtml;
+        namePh.innerText = `${photographer.name}`;
+        // createProfilUnique.innerHTML= contenuHtml
+
+        //div secondaire
+        const containterDescription = document.createElement("div");
+        containterDescription.className = "container_description_flex";
+        console.log(containterDescription);
+
+        //containerForWidth
+        const containerForWidth = document.createElement("div");
+        containerForWidth.className = "container_for_width";
+
+        //<h2
+        const createTitle = document.createElement("h2");
+        createTitle.className = "name_profil";
+        createTitle.innerHTML = `${photographer.name}`;
+        // <p ville
+        const createParagrapheVille = document.createElement("p");
+        createParagrapheVille.className = "ville_profil";
+        createParagrapheVille.innerHTML = `${photographer.city}, ${photographer.country}`;
+
+        //<p txt profil
+        const createParaProfil = document.createElement("p");
+        createParaProfil.className = "txt_profil";
+        createParaProfil.innerHTML = `${photographer.tagline}`;
+
+        // <ul list tag
+        const createListTag = document.createElement("ul");
+        createListTag.className = "filter";
+
+        // <li listUnique
+        photographer.tags.forEach((tag) => {
+          const createList = document.createElement("li");
+          createList.innerHTML = `${tag}`;
+          createListTag.appendChild(createList);
+        });
+
+        //containerBtn
+        const divBtn = document.createElement("div");
+        divBtn.className = "container_btn";
+
+        //btn
+        const createBtn = document.createElement("button");
+        createBtn.id = "btnForm";
+        const txtBtn = document.createTextNode("Contactez-moi");
+        createBtn.appendChild(txtBtn);
+        //display formulaire
+        createBtn.addEventListener("click", () => {
+          const form = document.getElementById("formContact");
+          const listPhotos = document.getElementById("contain_picture");
+          form.style.display = "block";
+          listPhotos.style.display = "none";
+        });
+
+        // <div img
+        const divImg = document.createElement("div");
+        divImg.className = "container_img";
+
+        // <img
+        const createImg = document.createElement("img");
+        createImg.src = `${photographer.portrait}`;
+        createImg.alt = `Photo de ${photographer.name}`;
+
+        //placement des création
+        containterDescription.appendChild(containerForWidth);
+        containerForWidth.appendChild(createTitle);
+        containerForWidth.appendChild(createParagrapheVille);
+        containerForWidth.appendChild(createParaProfil);
+        containerForWidth.appendChild(createListTag);
+        containterDescription.appendChild(divBtn);
+        divBtn.appendChild(createBtn);
+        divImg.appendChild(createImg);
+        createProfilUnique.appendChild(containterDescription);
+        createProfilUnique.appendChild(divImg);
       });
     });
   }
 }
+
+new listPhotos().displayPicture();
+new form().displayForm();
+new form().valdation();
