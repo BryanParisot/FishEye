c3207e71530cd345b8289a1476ada9ab2bf0339d
Bryan <bryanparisot@hotmail.com>
2021-10-20 10:50:59 +0200

    :sparkles: display photo and like and price

---
+++ js/photographeUnique/listPhotos.js
@@ -0,0 +1,86 @@
+import dataApi from "../dataUser/dataApi.js";
+
+export default class listPhotos {  
+
+    constructor(){
+      this.totalLike = 0 
+    };
+
+  displayPicture() {
+
+    new dataApi().findData().then((response) => {
+      response.media.map((media) => {
+        let id = window.location.search.split("id=")[1];
+        
+        if (id != media.photographerId) {
+          console.log("user different");
+          return false;
+        }
+         this.totalLike += media.likes
+
+        const createDivPictures = document.createElement("div");
+        const divContainPicture = document.getElementById("contain_picture");
+        createDivPictures.className = "container_card";
+        divContainPicture.appendChild(createDivPictures);
+
+        //element dom
+        //lien de l'image
+        const lien = document.createElement("a");
+        lien.href = `${media.image}`;
+        //img
+        const img_photographe = document.createElement("img");
+        img_photographe.className = "card_picture";
+        img_photographe.src = `${media.image}`;
+        img_photographe.alt = `${media.title}`;
+
+        const container_card_info = document.createElement("div");
+        container_card_info.className = "container_card_info";
+
+        //name
+        const name = document.createElement("p");
+        name.className = "name";
+        name.innerHTML = `${media.title}`;
+
+        // div like
+        const container_like = document.createElement("div");
+        container_like.className = "like_card";
+
+        //number like
+        const like = document.createElement("p");
+        like.className = "like";
+        like.innerHTML = `${media.likes}`;
+        like.id = 'numberLikes-'+ media.id
+
+        const heart = document.createElement("i");
+        heart.className = "fas fa-heart";
+        heart.id= `likeHeart${media.id}`
+        heart.setAttribute('data-like-id', media.id);
+        heart.addEventListener('click', this.addLikes) 
+
+        //ajout des element dans le dom
+        createDivPictures.appendChild(lien);
+        lien.appendChild(img_photographe);
+        createDivPictures.appendChild(container_card_info);
+        container_card_info.appendChild(name);
+        container_card_info.appendChild(container_like);
+        container_like.appendChild(like);
+        container_like.appendChild(heart);
+
+        const total = document.getElementById('like_total')
+        total.innerHTML = `${this.totalLike}`
+
+      });
+    });
+  }
+  addLikes = (e) => {
+    this.totalLike += 1
+    const total = document.getElementById('like_total')
+    total.innerHTML = `${this.totalLike}`
+    const heartP = document.getElementById(`numberLikes-${e.currentTarget.getAttribute("data-like-id")}`)
+    console.log(heartP)
+    
+    heartP.innerHTML = parseInt(heartP.textContent)+1
+
+    console.log(e.currentTarget)
+  }
+}
