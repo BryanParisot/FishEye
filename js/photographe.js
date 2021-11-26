d924ef5dd8e3974a2f2274c9da219c12b399fe38
Bryan <bryanparisot@hotmail.com>
2021-11-26 18:03:20 +0100

    :memo: add photographer js

---
+++ js/photographe.js
@@ -0,0 +1,9 @@
+import profil from "./photographeUnique/profil.js";
+import Form from "./photographeUnique/formulaire.js";
+import dropDown from "./photographeUnique/dropDown.js";
+
+
+new profil().displayProfil();
+new Form().validation();
+new dropDown().tri();
+
